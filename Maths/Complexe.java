deafe46157a2cfb71f997b0b346d1ffab0c98f56
alexphil12 <56894822+alexphil12@users.noreply.github.com>
2021-11-04 15:22:42 +0100

    Add files via upload

--- Maths/Complexe.java
+++ Maths/Complexe.java
@@ -4,13 +4,38 @@ public class Complexe {
     private double re;
     private double im;
 
+
+    public static Complexe[] fill_tab(Complexe[] sig){
+        int N=sig.length;
+        for(int k=0;k<N;k++){
+            sig[k]=new Complexe(Math.random(),Math.random());
+        }
+        return(sig);
+    }
     public Complexe(double re,double im){
         this.re=re;
         this.im=im;
     }
+    public void setRe(double re){
+        this.re=re;
+    }
+    public void setIm(double im){
+        this.im=im;
+    }
+    public double getRe(){
+        return(re);
+    }
+    public double getIm(){
+        return(im);
+    }
+    public Complexe(){
+        this.re=0;
+        this.im=0;
+    }
     public Complexe add(Complexe Z){
         return(new Complexe(this.re+Z.re,this.im+Z.im));
     }
+    public Complexe minus(Complexe Z){return((new Complexe(this.re-Z.re,this.im-Z.im)));}
     public Complexe Multi(Complexe Z){
         return(new Complexe(this.re*Z.re-this.im*Z.im,this.re*Z.im+this.im*Z.re));
     }
@@ -19,18 +44,25 @@ public class Complexe {
         return(this.Multi(h));
 
     }
-    public double module(Complexe z){
-        return(Math.sqrt(z.re*z.re+z.im*z.im));
+    public double module() {
+        return(Math.sqrt(re*re+im*im));
     }
-    public double argument(Complexe z){
+
+    public double argument(){
         return Math.atan2(im, re);
     }
-    public String ToString() {
+    public String toString() {
             if (im == 0) return re + "";
             if (re == 0) return im + "i";
             if (im <  0) return re + " - " + (-im) + "i";
             return re + " + " + im + "i";
         }
+    public static void main(String args[]){
+        Complexe h=new Complexe(-1,0);
+        double k=h.argument();
+        System.out.println(k);
+
+    }
 
 
 }
---
+++ Maths/FFT.java
@@ -0,0 +1,67 @@
+package Maths;
+
+import java.util.Arrays;
+
+import static Maths.Complexe.fill_tab;
+
+public class FFT {
+    public static Complexe[] fft(Complexe[] x) {
+        int n = x.length;
+        // Implémentation récursive
+        // cas n=1
+        if (n == 1) return new Complexe[] { x[0] };
+
+        // Algorithme FFT de Cooley Turkey
+        if (n % 2 != 0) {
+            throw new IllegalArgumentException("n n'est pas une puissance de 2");
+        }
+
+        // compute FFT of even terms
+        Complexe[] paire = new Complexe[n/2];
+        for (int k = 0; k < n/2; k++) {
+            paire[k] = x[2*k];
+        }
+        Complexe[] paireFFT = fft(paire);
+
+        // compute FFT of odd terms
+        Complexe[] impaire  = new Complexe[n/2];
+        for (int k = 0; k < n/2; k++) {
+            impaire[k] = x[2*k + 1];
+        }
+        Complexe[] impaireFFT = fft(impaire);
+
+        // combine
+        Complexe[] y = new Complexe[n];
+        for (int k = 0; k < n/2; k++) {
+            double kth = -2 * k * Math.PI / n;
+            Complexe wk = new Complexe(Math.cos(kth), Math.sin(kth));
+            y[k]       = paireFFT[k].add (wk.Multi(impaireFFT[k]));
+            y[k + n/2] = paireFFT[k].minus(wk.Multi(impaireFFT[k]));
+        }
+        return y;
+    }
+    public static Complexe[] DoubletoComplexe(double [] input_reel){
+        int n=input_reel.length;
+        Complexe[] y=new Complexe[n];
+        y=fill_tab(y);
+        for(int k=0;k<n;k++){
+            y[k].setRe(input_reel[k]);
+            y[k].setIm(0);
+        }
+        return (y);
+    }
+    public static double[] modulefft(Complexe [] sig){
+        int n=sig.length;
+        double[] y=new double[n];
+        for(int k=0;k<n;k++){
+            y[k]=sig[k].module();
+        }
+        return(y);
+    }
+    public static void main(String args[]){
+        double [] x={0,1,0,1};
+        Complexe[] x1=DoubletoComplexe(x);
+        Complexe[]y=fft(x1);
+        System.out.print("Y(0)="+y[0]+"Y(1)="+y[1]+"Y(2)="+y[2]+"Y(3)="+y[3]);
+    }
+}
