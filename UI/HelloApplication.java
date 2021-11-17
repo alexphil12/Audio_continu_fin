8eacec425a67924655c02c414ed1058252cbc4c7
alexphil12 <56894822+alexphil12@users.noreply.github.com>
2021-11-17 11:42:10 +0100

    Add files via upload

--- UI/HelloApplication.java
+++ UI/HelloApplication.java
@@ -3,10 +3,12 @@ package UI;
 
 import Audio.AudioIO;
 import Audio.AudioProcessor;
+import javafx.animation.AnimationTimer;
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Node;
 import javafx.scene.Scene;
+import javafx.scene.canvas.Canvas;
 import javafx.scene.chart.*;
 import javafx.scene.control.*;
 import javafx.scene.layout.BorderPane;
@@ -41,26 +43,29 @@ public class HelloApplication extends Application {
     private Node createToolbar() {
         Button button = new Button("Lance le processor");
         Button button1 = new Button("lance la fft");
-        Button button2=new Button ("Arrête le processor");
+        Button button2 = new Button("Arrête le processor");
         ToolBar tb = new ToolBar(button, new Label("Sélection de l'input"), new Separator());
         ComboBox<String> cbin = new ComboBox<>();
         ComboBox<String> cbout = new ComboBox<>();
-        ComboBox<Number> cbfreq=new ComboBox<>();
-        ComboBox<Number> cbtemps=new ComboBox<>();
-        cbfreq.getItems().addAll(2000,4000,8000,16000);//les valeurs sont telles que freq_ech*temps_frame/1000=2^n pour que le calcul de la fft soit toujours possible.
-        cbtemps.getItems().addAll(256,512,1024);
-        cbout.getItems().addAll("Default Audio Device", "Port Speakers (Realtek(R) Audio)","Périphérique audio principal","Speakers (Realtek(R) Audio)");
-        cbin.getItems().addAll("Default Audio Device", "Microphone Array (Realtek(R) Audio)","Pilote de capture audio principal","Port Microphone Array (Realtek(R) Au");
-        tb.getItems().addAll(cbin, button1, new Label("Sélection de l'output"), cbout,button2,new Label("Sélection de la fréquence(hz)"),cbfreq,new Label("Sélection du temps de frame (ms)"),cbtemps);
+        ComboBox<Number> cbfreq = new ComboBox<>();
+        ComboBox<Number> cbtemps = new ComboBox<>();
+        cbfreq.getItems().addAll(2000, 4000, 8000, 16000);//les valeurs sont telles que freq_ech*temps_frame/1000=2^n pour que le calcul de la fft soit toujours possible.
+        cbtemps.getItems().addAll(256, 512, 1024, 2048);
+        cbout.getItems().addAll("Default Audio Device", "Port Speakers (Realtek(R) Audio)", "Périphérique audio principal", "Speakers (Realtek(R) Audio)");
+        cbin.getItems().addAll("Default Audio Device", "Microphone Array (Realtek(R) Audio)", "Pilote de capture audio principal", "Port Microphone Array (Realtek(R) Au");
+        tb.getItems().addAll(cbin, button1, new Label("Sélection de l'output"), cbout, button2, new Label("Sélection de la fréquence(hz)"), cbfreq, new Label("Sélection du temps de frame (ms)"), cbtemps);
         button.setOnAction(event -> {
             try {
-                AudioIO.start_Audio_Processing(cbin.getValue(),cbout.getValue(),(int)cbfreq.getValue(),(int)cbtemps.getValue());
+                AudioIO.start_Audio_Processing(cbin.getValue(), cbout.getValue(), (int) cbfreq.getValue(), (int) cbtemps.getValue());
             } catch (LineUnavailableException e) {
                 e.printStackTrace();
             }
+            System.out.println("début de l'audio processing");
         });
-        button2.setOnAction(actionEvent -> {AudioIO.stop_Audio_Processing();});
-        button1.setOnAction(actionEvent -> {AudioProcessor.Lance_fft(sigfreq,true);});
+        button2.setOnAction(actionEvent -> {
+            AudioIO.stop_Audio_Processing();
+            System.out.println("fin de l'audio processing");
+        });
         return tb;
     }
 
@@ -71,28 +76,60 @@ public class HelloApplication extends Application {
     }
 
     private Node createMainContent() {
-        Region I=new Region();
-        Pane u=new Pane();
-        NumberAxis xAxisfreq = new NumberAxis(0, 4000, 100);
+        Region I = new Region();
+        Pane u = new Pane();
+        Button b=new Button("lance fft");
+        NumberAxis xAxisfreq = new NumberAxis(0, 4096, 10);
         xAxisfreq.setLabel("Fréquence en hertz");
-        NumberAxis yAxisfreq = new NumberAxis(0, 2, 0.01);
+        NumberAxis yAxisfreq = new NumberAxis(0, 15, 0.5);
         yAxisfreq.setLabel("Amplitude du Spectre");
         SignalView sigfreq = new SignalView(xAxisfreq, yAxisfreq);
+        Group h = new Group(sigfreq,b);
+        Thread thread=new Thread(){
+            @Override
+            public void run(){
+                AnimationTimer timer = new AnimationTimer() {
+                    @Override
+                    public void handle(long now) {
+                        AudioIO.Update_data_fft(true,sigfreq);
+                    }
 
-        Group h = new Group(sigfreq);
+                };
+                timer.start();
+            }
+        };
+        b.setOnAction(actionEvent -> {thread.start();System.out.println("traitement fft lancé");});
         return (h);
         // ici en utilisant g.getChildren().add(...) vous pouvez ajouter tout élément graphique souhaité de type Nod
     }
 
     private Node createSigtemp() {
-        NumberAxis xAxis = new NumberAxis(0, 2048, 16);
+        Button b = new Button("lance temp");
+        NumberAxis xAxis = new NumberAxis(0, 4096 ,16);
         xAxis.setLabel("Nombre d'échantillon");
-        NumberAxis yAxis = new NumberAxis(-1, 1, 0.01);
+        NumberAxis yAxis = new NumberAxis(-1, 1, 0.1);
         yAxis.setLabel("intensité du signal en sortie");
         SignalView sigtemp = new SignalView(xAxis, yAxis);
-        Group k = new Group(sigtemp);
+        Canvas u=new Canvas(2,15);
+        u.getGraphicsContext2D();
+        Group k = new Group(sigtemp,b,u);
+        Thread thread=new Thread(){
+            @Override
+            public void run(){
+                AnimationTimer timer = new AnimationTimer() {
+                    @Override
+                    public void handle(long now) {
+                        AudioIO.Update_data_sig(true,sigtemp);
+                    }
+
+                };
+                timer.start();
+            }
+        };
+        b.setOnAction(actionEvent -> {thread.start();System.out.println("traitement temps lancé");});
         return k;
     }
+
     private Node createSpectro() {
         NumberAxis xAxis = new NumberAxis(0, 10, 1);
         xAxis.setLabel("Temps");
@@ -103,6 +140,7 @@ public class HelloApplication extends Application {
         return k;
     }
 }
+
 
 
 
--- UI/SignalView.java
+++ UI/SignalView.java
@@ -5,26 +5,16 @@ import javafx.scene.chart.Axis;
 import javafx.scene.chart.*;
 import javafx.animation.AnimationTimer;
 
+import static Audio.AudioProcessor.inputSignal;
+
 public class SignalView extends LineChart {
     public SignalView(Axis<Number> axis, Axis<Number> axis1) {
         super(axis, axis1);
     }
     private XYChart.Series tab;
 
+    public static void main(String args[]){
 
-    public void Update_data_sig_temp(Boolean update,AudioSignal sig,SignalView view) {
-        new AnimationTimer() {
-            @Override
-            public void handle(long l) {
-                XYChart.Series serie=new XYChart.Series();
-                if(update==true){
-                    for(int k=0;k<sig.getSampleBuffer().length;k++){
-                        serie.getData().add(new XYChart.Data(k,sig.getSample(k)));
-                    }
-                    view.getData().add(serie);
-                }
-            }
-        };
     }
 
 }
