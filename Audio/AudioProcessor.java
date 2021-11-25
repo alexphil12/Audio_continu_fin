f5a414f2addf7bc376ac810899f6541246d3df61
alexphil12 <56894822+alexphil12@users.noreply.github.com>
2021-11-25 23:45:15 +0100

    Add files via upload

--- Audio/AudioProcessor.java
+++ Audio/AudioProcessor.java
@@ -38,7 +38,7 @@ public class AudioProcessor implements Runnable {
              if(fonctio=="Sortie par défaut"){
              outputSignal.setFrom(inputSignal);}
              if(fonctio=="Filtre_passe_bas"){
-                 filtre_passe_bas(inputSignal,1000,(double)inputSignal.getFrameSize().getFreq_ech(), outputSignal);}
+                 filtre_passe_bas(inputSignal,500,(double)inputSignal.getFrameSize().getFreq_ech(), outputSignal);}
              if(fonctio=="Filtre_passe_haut"){
                  filtre_passe_haut(inputSignal,2000,(double)inputSignal.getFrameSize().getFreq_ech(),outputSignal);}
              if(fonctio=="écho"){
